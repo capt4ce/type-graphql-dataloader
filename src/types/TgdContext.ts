2d92a5346d96dd77fbfb0bc3fa5db8a8bd7e8a75
Ahmad Ali Abdilah <ahmad.ali.abdilah@gmail.com>
2025-02-13 11:58:33 +0800

    feat: allow to use master replica if the parent operation is mutation

--- src/types/TgdContext.ts
+++ src/types/TgdContext.ts
@@ -1,6 +1,7 @@
-import type { Connection } from "typeorm";
+import type { Connection, ReplicationMode } from "typeorm";
 
 export interface TgdContext {
   requestId: string;
   typeormGetConnection?: () => Connection;
+  mutationReplica?: ReplicationMode;
 }
